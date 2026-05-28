7945b87ba6338c3da9df566d1f2eb0817db5f433
Bharathgopalsamy <bharathgopalswamy09@gmail.com>
2026-05-28 12:32:41 -0300

    fix deployed api base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://dermaai-yu8u.onrender.com",
+  baseURL: "https://dermaai-yu8u.onrender.com/api",
 });
 
 export default API;
